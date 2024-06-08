9791249d8fa6f38a18f2029ccd530f60a1db5be5
Boris Garmev <borisgarmev912@proton.me>
2024-06-08 11:55:01 +0300

    Initial Release
    HTML, CSS, JS, Assets etc.

---
+++ js/default.js
@@ -0,0 +1,6 @@
+function toggleHeadbar() {
+    var headbar = document.querySelector("headbar");
+    var header = document.querySelector("top");
+    headbar.classList.toggle("headbar-open");
+    header.classList.toggle("top-headbar-open");
+}
